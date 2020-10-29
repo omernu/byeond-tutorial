da55cd0916b2ef9f499ec49c93f6444656e77005
Omer Nuri <omernu88@gmail.com>
2020-10-29 22:31:56 +0200

    this is my massage

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 # The -e option would make our script exit with an error if any command
 # fails while the -x option makes verbosely it output what it does
+
 # Install Pipenv, the -n option makes sudo fail instead of asking for a
 # password if we don't have sufficient privileges to run it
 sudo -n dnf install -y pipenv
@@ -9,4 +10,4 @@ cd /vagrant
 pipenv sync
 # run our app. Nohup and "&" are used to let the setup script finish
 # while our app stays up. The app logs will be collected in nohup.out
-nohup pipenv run python manage.py runserver 0.0.0.0:8000 &
+nohup pipenv run python ./manage.py runserver 0.0.0.0:8000 &
